54773490d8a0ff2d9eb644925b22516ae730e367
Clark <clarkhyde123@gmail.com>
2021-05-27 12:20:10 -0500

    made a basic redux

--- src/index.js
+++ src/index.js
@@ -1,12 +1,10 @@
-// import { compose, pipe } from 'lodash/fp';
-// import { produce } from 'immer';
-import store from './store';
+import store from './customStore';
 import * as actions from './actions';
 
-
+store.subscribe(()=>{
+    console.log("Store changed");
+})
 store.dispatch(actions.bugAdded("Bug 1"));
-
-store.dispatch(actions.bugResolved(1));
 
 console.log(store.getState());
 
