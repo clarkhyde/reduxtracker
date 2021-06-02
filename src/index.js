2c0f8eb6a6b16ebc189f46b8f8094e4e6f503553
Clark <clarkhyde123@gmail.com>
2021-06-02 11:26:39 -0500

    redux in ducks format

--- src/index.js
+++ src/index.js
@@ -1,10 +1,13 @@
-import store from './customStore';
-import * as actions from './actions';
+import configureStore from './store/configureStore.js';
+import * as actions from './store/bugs.js';
 
+const store = configureStore();
 store.subscribe(()=>{
     console.log("Store changed");
 })
 store.dispatch(actions.bugAdded("Bug 1"));
-
+store.dispatch(actions.bugAdded("Bug 2"));
+store.dispatch(actions.bugAdded("Bug 3"));
+store.dispatch(actions.bugResolved(1));
 console.log(store.getState());
 
